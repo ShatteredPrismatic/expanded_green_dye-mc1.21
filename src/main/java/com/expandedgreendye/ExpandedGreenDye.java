3cd4958167db52db2dd085c31f8a96a15d8e3e57
ShatteredPrismatic <rubysystem404@gmail.com>
2024-07-23 17:12:32 -0500

    updated moss_clump.png to make it fitter better with moss; thinking of removing vine recipes

--- src/main/java/com/expandedgreendye/ExpandedGreenDye.java
+++ src/main/java/com/expandedgreendye/ExpandedGreenDye.java
@@ -2,6 +2,7 @@ package com.expandedgreendye;
 import com.expandedgreendye.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.item.ItemGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,7 +10,7 @@ public class ExpandedGreenDye implements ModInitializer {
 	public static final String MOD_ID = "expanded_green_dye";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-	@Override
+    @Override
 	public void onInitialize() {
 		ModItems.AddToGroups();
 	}
--- src/main/java/com/expandedgreendye/item/ModItems.java
+++ src/main/java/com/expandedgreendye/item/ModItems.java
@@ -1,13 +1,17 @@
 package com.expandedgreendye.item;
 
 import com.expandedgreendye.ExpandedGreenDye;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class ModItems {
@@ -28,4 +32,9 @@ public class ModItems {
                 .register((itemGroup) -> itemGroup.add(ModItems.MOSS_CLUMP));
         FuelRegistry.INSTANCE.add(ModItems.MOSS_CLUMP, 15 * 20);
     }
+    public static final RegistryKey<ItemGroup> MossClumpGroupKey = RegistryKey.of(Registries.ITEM_GROUP.getKey(), Identifier.of(ExpandedGreenDye.MOD_ID, "moss_clump_group"));
+    public static final ItemGroup MossClumpGroup = FabricItemGroup.builder()
+            .icon(() -> new ItemStack(ModItems.MOSS_CLUMP))
+            .displayName(Text.translatable("itemGroup.expanded_green_dye"))
+            .build();
 }
